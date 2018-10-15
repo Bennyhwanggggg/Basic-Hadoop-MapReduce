83125f4673bc69b73ebdcffdd21e086b76e8a460
Benny Hwang <BennyHwanggggg@user.noreply.github.com>
2018-10-16 00:41:02 +1100

    proj3 help

---
+++ Lab7/src/comp9313/lab7/Problem2.scala
@@ -0,0 +1,38 @@
+package comp9313.lab7
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd._
+
+object Problem2 {
+  var Id = 0
+  var PostId = 1
+  var VoteTypeId = 2
+  var UserId = 3
+  var CreationTime = 4
+  
+  def Q2(textFile: RDD[Array[String]]){
+    val puPair = textFile.filter( _(VoteTypeId) == "5").map( x=> (x(PostId), x(UserId))).distinct()
+    val res = puPair.groupByKey().filter(_._2.size>10).mapValues(x => x.toList.sortBy(_.toInt)).sortBy(_._1.toInt)
+    val fmtres = res.map{case (x, y) => s"""$x#${y.mkString(",")}"""}
+    fmtres.foreach(println)
+  }
+  
+  def Q1(textFile: RDD[Array[String]]) {
+    val pvPair = textFile.map( x=> (x(VoteTypeId), x(PostId))).distinct()
+    val res = pvPair.countByKey().toSeq.sortWith(_._2 > _._2).take(5)
+    res.foreach( x => println(x._1+"\t"+x._2))
+  }
+  
+  def main(args: Array[String]) {
+    val inputFile = args(0)
+    val outputFolder = args(1)
+    val conf = new SparkConf().setAppName("Lab 8 Problem2").setMaster("local")
+    val sc = new SparkContext(conf)
+    val textFile = sc.textFile(inputFile).map(_.split(","))
+    
+    Q1(textFile)
+    Q2(textFile)
+  }
+}
---
+++ Project2/src/comp9313/proj2/Problem1.scala
@@ -0,0 +1,40 @@
+package comp9313.proj2
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd._
+
+
+object Problem1 {
+  
+  def main(args: Array[String]) {
+    val inputFile = args(0)
+    val outputFolder = args(1)
+    val conf = new SparkConf().setAppName("Project 1 problem 1").setMaster("local")
+    val sc = new SparkContext(conf)
+    val textFile = sc.textFile(inputFile)
+
+    val lines = textFile.map( x => x.toLowerCase.split("[\\s*$&#/\"'\\,.:;?!\\[\\](){}<>~\\-_]+").
+         filter( x=> x.length >= 1).filter(x => x.charAt(0) <= 'z' && x.charAt(0) >= 'a'))
+ 
+    val pairs = lines.map( x => for(i <- 0 to x.length-1; j <- i+1 to x.length-1) yield (x(i), x(j)))
+    
+    // (pair, sum)
+    val coOccurCount = pairs.map( x=> x.toList).flatMap(x => x.map(x => (x, 1))).
+                          reduceByKey(_+_)
+                
+    // (word, sum)                      
+    val totalCount = coOccurCount.map( x => (x._1._1, x._2)).reduceByKey(_+_)
+    val coOccurCountModified = coOccurCount.map( x => (x._1._1, (x._1, x._2)))
+
+    // (word, (wordcount, (pair, paircount))
+    val joined = totalCount join coOccurCountModified
+   
+    // sort by first word, value, then second word
+    val relativeFrequency = joined.map( x => (x._2._2._1, (x._2._2._2.toDouble/x._2._1.toDouble))).sortByKey().sortBy{ case (k, v) => (k._1, -v) }
+    
+    val formatedResult = relativeFrequency.map( x => x._1._1 + " " + x._1._2 + " " + x._2).saveAsTextFile(outputFolder)
+
+  }
+}
